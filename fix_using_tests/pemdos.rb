fb67749a50e61d95b4734008ce592b997b831fab
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-05 21:18:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry' 
 def snake_it_up(string)
-  empty_string = ""
   if string[0] == "s"
-    10.times {"s" + empty_string}
-    string = empty_string + string 
-    string
+    10 * "s" + empty_string}
   else
     string
   end
